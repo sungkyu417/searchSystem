855e81e2d03b0b35382e527fa3623abd70d3b2d1
Administrator <Administrator@PPTV3-20161011X>
2016-10-12 16:09:01 +0800

    lll

---
+++ vue-demo/demo/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+	<p>
+		{{message}}
+	</p>
+</template>
+<script>
+	export default {
+		data(){
+		return {
+			message:"Hello World!"
+		}
+		}
+	}
+</script>
---
+++ vue-demo/demo/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App'
+//import Hello from './components/Hello.vue'
+//
+//import VueRouter from 'vue-router'
+//import VueResource from 'vue-resource'
+//
+//Vue.use(VueResource)
+//Vue.use(VueRouter)
+//
+//const router=new VueRouter()
+//
+//router.map({
+//	'/hello':{
+//		component:Hello
+//	}
+//})
+//router.redirect({
+//	'*':'/hello'
+//})
+//router.start(App,'#app')
+
+/* eslint-disable no-new */
+new Vue({
+el: '#app',
+render: h => h(App)
+})
